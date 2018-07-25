96497ae7e736606502f44770acf84ee1de37bf07
Julmar <julmarfigueroa@gmail.com>
2018-07-25 14:40:34 -0400

    Installed fs, cheerio, axios

---
+++ app.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+// 1. fetch the site
+// 2. grabImages
+// 3. saveImages
+// 4. put them all together
